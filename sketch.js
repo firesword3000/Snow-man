33fdcb7bf7ee152d6c2c48bbe3ecc1c1ad600114
firesword3000 <samahsummah@gmail.com>
2016-08-04 08:43:34 -0500

    Snumin

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+function setup(circle) {
+  createCanvas(1200,1600);
+  fill(255,255,255);
+  noStroke()
+  background(0,128,285)
+  ellipse(500,350,300);
+  ellipse(500,200,150)
+  ellipse(500,100,100)
+  fill(0,0,0)
+  rect(475,20,50,35)
+  fill(34,34,34)
+  rect(455,60,90,10)
+  ellipse(470,100, 20)
+  ellipse(530,100,20)
+  fill(255,77,255)
+  ellipse(500,225,30)
+  ellipse(500,350,45)
+  ellipse(500,280,35)
+  ellipse(500,155,20)
+}
+function circle(x, y, diameter) {
+  ellipse(x,y,w,h)
+  
+}
